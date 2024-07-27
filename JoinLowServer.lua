5578a5b6b4e1aca73137e1fa98f6c909f2e1063d
rdx222f <166171248+rdx222f@users.noreply.github.com>
2024-07-27 12:28:34 -0500

    JoinLowServer.lua

---
+++ JoinLowServer.lua
@@ -0,0 +1,9 @@
+local http = game:GetService("HttpService")
+local tps = game:GetService("TeleportService")
+local Api = "https://games.roblox.com/v1/games/"
+
+local _place = game.PlaceId
+local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
+function ListServers(cursor)
+   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+   return http:JSONDecode(Raw)
